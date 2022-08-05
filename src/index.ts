c18739236cee672f523d5ae2a92b58d5c3656521
Xander Dyer <xander.dyer@hotmail.com>
2022-08-04 21:05:03 -0400

    changing port env

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { createApp } from './utils/createApp';
 import './database'
 
 const app= express();
-const PORT = process.env.Port || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 async function main() {
