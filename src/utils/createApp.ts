31e094605ebbc005f5aa5bc990a603624e2c3261
Xander Dyer <xander.dyer@hotmail.com>
2022-08-06 22:52:50 -0400

    add domain to express session

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -27,6 +27,7 @@ export function createApp(): Express {
         cookie: {
             maxAge: 60000 * 60 * 24 * 7,
             secure: false,
+            domain: 'jasn.site'
         },
         store: store.create({
             mongoUrl: `${DB}`,
